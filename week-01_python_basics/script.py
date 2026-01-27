d4a69ab2f64eed86b6b482f2648a0eaa7e5adf57
FabTerrat <fterrat@ensc.fr>
2026-01-27 10:13:28 +0100

    Filtrage score + Réécriture mail en minuscule

--- week-01_python_basics/script.py
+++ week-01_python_basics/script.py
@@ -1,3 +1,5 @@
+#------------------Ouverture et lecture du fichier CS--------------------------
+
 import csv
 
 INPUT_FILE = "input.csv"
@@ -8,3 +10,17 @@ with open (INPUT_FILE, mode="r", encoding="utf-8", newline="") as f:
 
 print("Nombre de lignes lues :", len(rows))
 print("Première ligne:", rows[0])
+
+
+#------------------filtrage des scores-------+Réécriture du mail en minuscule-------------------
+filtered_rows = []
+
+for row in rows:
+    score = int(row["score"])
+
+    if score >= 80 :
+        row["email"] = row["email"].lower()
+        filtered_rows.append(row)
+
+print("Lignes après filtrage :", len(filtered_rows))
+print("Exemple ligne filtrée", filtered_rows[0])
